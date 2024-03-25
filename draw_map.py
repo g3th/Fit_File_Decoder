f9ab990eec293d09ace373cf7c7e2878616ae955
g3th <theinkandpen@yahoo.co.uk>
2024-03-25 06:57:25 +0000

    added: folium map drawing module

---
+++ draw_map.py
@@ -0,0 +1,27 @@
+import folium
+import webbrowser
+from folium import IFrame
+
+class DrawMap:
+
+    def __init__(self, start, end, colour, zoom_value, width, w, h):
+        self.zoom_value = zoom_value
+        self.line_width = width
+        self.colour = colour
+        self.folium_map_object = folium.Map(width=w, height=h, location=[start, end], zoom_start=self.zoom_value)
+        self.webbrowser = webbrowser
+
+    def create_points(self, lon, lat):
+        folium.PolyLine([lon, lat], color=self.colour, weight=self.line_width, opacity=0.8).add_to(self.folium_map_object)
+
+    def popup_with_run_info(self, val):
+        self.folium_map_object.get_root().html.add_child(folium.Element("""
+            <div style="position:fixed;left:1200px;width:200px;height:200px;background-color:white;z-index:900">
+            <h1><br> {} </br></h1>
+            </div>
+        """.format(val)))
+    def draw(self):
+        self.folium_map_object.save("route.html")
+        self.webbrowser.open("route.html")
+
+
