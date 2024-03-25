a1d4002c70644ccb4e578667b5119ebca5f352e3
g3th <theinkandpen@yahoo.co.uk>
2024-03-25 18:59:27 +0000

    change colour halfway through the route

--- draw_map.py
+++ draw_map.py
@@ -4,15 +4,14 @@ from folium import IFrame
 
 class DrawMap:
 
-    def __init__(self, start, end, colour, zoom_value, width, w, h):
+    def __init__(self, start, end, zoom_value, line_width, w, h):
         self.zoom_value = zoom_value
-        self.line_width = width
-        self.colour = colour
+        self.line_width = line_width
         self.folium_map_object = folium.Map(width=w, height=h, location=[start, end], zoom_start=self.zoom_value)
         self.webbrowser = webbrowser
 
-    def create_points(self, lon, lat):
-        folium.PolyLine([lon, lat], color=self.colour, weight=self.line_width, opacity=0.8).add_to(self.folium_map_object)
+    def create_points(self, lon, lat, colour):
+        folium.PolyLine([lon, lat], color=colour, weight=self.line_width, opacity=0.8).add_to(self.folium_map_object)
 
     def popup_with_run_info(self, val):
         self.folium_map_object.get_root().html.add_child(folium.Element("""
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from decoder import DecodeFile
 
 # Examples - will be command line arguments:
 
-disk = DiskOperations("3.fit") # FIT file name - must be placed in "fit_files" folder
+disk = DiskOperations("5.fit") # FIT file name - must be placed in "fit_files" folder
 bytes_list = disk.open_file() # Open file
 decoder = DecodeFile(bytes_list) # Decode Information
 distance = decoder.extract_distance() # Any method - i.e. distance, speed, average pace, title etc..
@@ -12,9 +12,12 @@ long_lat_list = decoder.extract_long_and_lat_and_convert_semicircles() # Get rou
 start_points = int((len(long_lat_list) - 1) / 2) # Start map roughly in the middle of the route
 
 # Add Folium Points and Generate
-generate_map = DrawMap(long_lat_list[start_points][0], long_lat_list[start_points][1], 'black', 12.5, 3, 1200, 1200)
+generate_map = DrawMap(long_lat_list[start_points][0], long_lat_list[start_points][1], 12.5, 3, 1200, 1200)
+colour = 'black'
 for i in range(len(long_lat_list)):
-    generate_map.create_points(long_lat_list[i], long_lat_list[i])
+    if i == int((len(long_lat_list) - 1) / 2):
+        colour = 'red'
+    generate_map.create_points(long_lat_list[i], long_lat_list[i], colour)
 # Inject HTML div into existing map html
 generate_map.popup_with_run_info("Total Distance: {} km".format(distance[len(distance) - 1]))
 # Draw
